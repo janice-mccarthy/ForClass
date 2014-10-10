925f3605dfc24a5995e70c36bf175802ff704064
janice-mccarthy <janice.mccarthy@duke.edu>
2014-10-10 14:45:14 -0400

    Change fig1 to uniform

--- R/fig1.R
+++ R/fig1.R
@@ -1,6 +1,6 @@
 ############## Begin R Script ##############
 library(ggplot2)
-data=data.frame(x=rnorm(100))
+data=data.frame(x=runif(100))
 plot=qplot(x, data=data, geom="histogram")
 ggsave(plot,file="../Figs/fig1.pdf")
 ############## End R Script ##############
